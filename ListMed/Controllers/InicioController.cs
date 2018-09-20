ebe3aa67db1db56e538d01fefee977af00b114df
Gabriel Abbade <gabriel@jdsystem.com.br>
2018-09-20 11:03:18 -0300

    começar a pegar

--- ListMed/Controllers/InicioController.cs
+++ ListMed/Controllers/InicioController.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using ListMed.DTO;
+using ListMed.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,9 +12,25 @@ namespace ListMed.Controllers
 {
     public class InicioController : Controller
     {
+        private MedListContext db = new MedListContext();
+        HttpClient client = new HttpClient();
+
+        public InicioController()
+        {
+            client.BaseAddress = new Uri("https://maps.googleapis.com");
+            
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
         // GET: Inicio
         public ActionResult Index()
         {
+            List<ClinicaDTO> clinicas = new List<ClinicaDTO>();
+            HttpResponseMessage response = client.GetAsync("/maps/api/place/findplacefromtext/json?input=clinica%20popular%20rio%20de%20janeiro&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry,place_id,price_level,permanently_closed&key=AIzaSyBbjKpIM4wD3dj3W5VqGuCYMH6hdoGhXP8").Result;
+            if (response.IsSuccessStatusCode)
+            {
+                string teste = response.Content.ReadAsStringAsync().Result;
+            }
             return View();
         }
     }
---
+++ ListMed/DTO/ClinicaDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ListMed.DTO
+{
+    public class ClinicaDTO
+    {
+        public string formatted_address { get; set; }
+        public geometry geometry { get; set; }
+        public string name { get; set; }
+        public opening_hours opening_hours { get; set; }
+        public double rating { get; set; }
+        public string place_id { get; set; }
+
+    }
+}
