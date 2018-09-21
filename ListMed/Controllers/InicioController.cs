50bd386661bdbed4cb220d3a4fb635a0c034a59a
gabbade <gabbade@gmail.com>
2018-09-20 23:49:02 -0300

    ,

--- ListMed/Controllers/InicioController.cs
+++ ListMed/Controllers/InicioController.cs
@@ -25,12 +25,12 @@ namespace ListMed.Controllers
         // GET: Inicio
         public ActionResult Index()
         {
-            List<ClinicaDTO> clinicas = new List<ClinicaDTO>();
-            HttpResponseMessage response = client.GetAsync("/maps/api/place/findplacefromtext/json?input=clinica%20popular%20rio%20de%20janeiro&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry,place_id,price_level,permanently_closed&key=AIzaSyBbjKpIM4wD3dj3W5VqGuCYMH6hdoGhXP8").Result;
-            if (response.IsSuccessStatusCode)
-            {
-                string teste = response.Content.ReadAsStringAsync().Result;
-            }
+            //List<ClinicaDTO> clinicas = new List<ClinicaDTO>();
+            //HttpResponseMessage response = client.GetAsync("/maps/api/place/findplacefromtext/json?input=clinica%20popular%20rio%20de%20janeiro&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry,place_id,price_level,permanently_closed&key=AIzaSyBbjKpIM4wD3dj3W5VqGuCYMH6hdoGhXP8").Result;
+            //if (response.IsSuccessStatusCode)
+            //{
+            //    string teste = response.Content.ReadAsStringAsync().Result;
+            //}
             return View();
         }
     }
