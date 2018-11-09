e3e1c47495fe3ee7c1e06f9b81265e1bf62bd08e
Gabriel Abbade <gabriel@jdsystem.com.br>
2018-11-09 10:11:49 -0200

    .

---
+++ ListMed/Migrations/201811091204025_teste.cs
@@ -0,0 +1,16 @@
+namespace ListMed.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class teste : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
